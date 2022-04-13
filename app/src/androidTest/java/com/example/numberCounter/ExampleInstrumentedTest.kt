bd920e5846f79366b5a42952667b8c3e901b68db
joye <ssu1019@naver.com>
2022-04-14 03:24:35 +0900

    [개발] fragment 화면 구성 (화면 안그림)

--- app/src/androidTest/java/com/example/numberCounter/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/numberCounter/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.numbercounter
+package com.example.numberCounter
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
